e57c524e7ff075acac07e6999ae7a8f9a5cc5971
RC <alinuxuser@fastmail.com>
2026-04-27 20:51:36 -0500

    misc

--- linux/scripts/kate_insert_date_time.sh
+++ linux/scripts/kate_insert_date_time.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # run this to make the script executable - chmod +x ~/git/ldk/linux/scripts/kate_insert_date_time.sh
-printf "%s" "$(date '+%b %d, %Y %A %I:%M:%S %p %Z')"
+# printf "%s" "$(date '+%b %d, %Y %A %I:%M:%S %p %Z')"
+printf "%s" "$(date '+%A, %B %-d, %Y, %-I:%M %p %Z')"
